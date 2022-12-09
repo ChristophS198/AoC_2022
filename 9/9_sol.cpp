c63482068755225b516275e2f960cb6fa4f73b09
Christoph Sommerauer <christoph.cs.sommerauer@gmail.com>
2022-12-09 19:40:00 +0100

    Finalize Day 9

--- 9/9_sol.cpp
+++ 9/9_sol.cpp
@@ -23,7 +23,7 @@ struct MoveOp
 };
 
 std::vector<MoveOp> read_in_move_operations(const std::string &file_path);
-std::uint32_t get_num_visited_fields(RopeField &field, const std::vector<MoveOp> &moves, std::int32_t x=0, std::int32_t y=0);
+std::uint32_t get_num_visited_fields(RopeField &field, const std::vector<MoveOp> &moves, std::int32_t x=0, std::int32_t y=0, size_t num_knots=1u);
 void follow_knot(const std::int32_t h_x, const std::int32_t h_y, std::int32_t &t_x, std::int32_t &t_y);
 
 std::uint32_t day_9_1(const std::string &file_path)
@@ -31,22 +31,25 @@ std::uint32_t day_9_1(const std::string &file_path)
     std::vector<MoveOp> move_ops = read_in_move_operations(file_path);
     RopeField field(DEFAULT_SIZE, std::vector<bool>(DEFAULT_SIZE,false));
     
-    return get_num_visited_fields(field, move_ops, 500, 500);
+    return get_num_visited_fields(field, move_ops, 500, 500, 1u);
 }
 
 std::uint32_t day_9_2(const std::string &file_path)
 {
-
+    std::vector<MoveOp> move_ops = read_in_move_operations(file_path);
+    RopeField field(DEFAULT_SIZE, std::vector<bool>(DEFAULT_SIZE,false));
     
-    return 0u;
+    return get_num_visited_fields(field, move_ops, 500, 500, 9u);
 }
 
 
-std::uint32_t get_num_visited_fields(RopeField &field, const std::vector<MoveOp> &moves, std::int32_t h_x, std::int32_t h_y)
+std::uint32_t get_num_visited_fields(RopeField &field, const std::vector<MoveOp> &moves, std::int32_t h_x, std::int32_t h_y, size_t num_knots)
 {
     std::uint32_t num_v{ 1u };
-    std::int32_t t_x{ h_x }; // tail x pos
-    std::int32_t t_y{ h_y }; // tail y pos
+    std::vector<std::int32_t> k_x(num_knots, h_x); // x pos of knots after head
+    std::vector<std::int32_t> k_y(num_knots, h_y); // y pos of knots after head
+    std::int32_t &t_x = k_x.back(); 
+    std::int32_t &t_y = k_y.back(); 
     field[t_x][t_y] = true;
 
     for (const auto &move : moves)
@@ -80,7 +83,11 @@ std::uint32_t get_num_visited_fields(RopeField &field, const std::vector<MoveOp>
                 std::string coords = "(x=" + std::to_string(h_x) + ",y=" + std::to_string(h_y) + ")";
                 throw std::runtime_error(coords + "\nIndex too large! Increase default field size.");
             }
-            follow_knot(h_x, h_y, t_x, t_y);
+            follow_knot(h_x, h_y, k_x.front(), k_y.front());
+            for (int32_t i=0; i<static_cast<int32_t>(num_knots)-1; ++i)
+            {
+                follow_knot(k_x.at(i), k_y.at(i), k_x.at(i+1), k_y.at(i+1));
+            }
             if (!field.at(t_x).at(t_y))
             {
                 field.at(t_x).at(t_y) = true;
--- 9/docu.md
+++ 9/docu.md
@@ -689,4 +689,6 @@ Now, the tail (9) visits 36 positions (including s) at least once:
 ```
 Simulate your complete series of motions on a larger rope with ten knots. How many positions does the tail of the rope visit at least once?
 
-Answer: 
+Your puzzle answer was 2793.
+
+Both parts of this puzzle are complete! They provide two gold stars: **
